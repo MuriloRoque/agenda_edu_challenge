bb532995720632a48e092b29b52a78a08d842b77
Murilo Roque <muriloengqui@gmail.com>
2020-08-03 14:26:43 -0300

    config update profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,5 +3,29 @@ module Api::V1
     def profile
       render json: @current_user
     end
+
+    def update
+      if user_params[:password].blank? || user_params[:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
+
+      if @current_user.update(user_params)
+        render json: @current_user
+      else
+        render json: @current_user.errors
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
   end
 end
--- spec/requests/patch_profile_spec.rb
+++ spec/requests/patch_profile_spec.rb
@@ -1,15 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "update a profile route", :type => :request do
-#   let!(:user) { User.create( name: 'Murilo',
-#                              email: 'murilo@email.com',
-#                              password: '123456',
-#                              password_confirmation: '123456') }
-# before {patch "/api/v1/profile?user[name]=Murilo2"}
-# it 'returns the updated profile' do
-#     expect(JSON.parse(response.body)['user']['name']).to eq('Murilo2')
-#   end
-# it 'returns status code 200' do
-#     expect(response).to have_http_status(:success)
-#   end
-# end
+describe 'update a profile route', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:user1) { FactoryBot.create(:user, :master) }
+  let!(:messages) { FactoryBot.create_list(:message, 20, from: user1.id, to: user.id) }
+  before do
+    patch 'http://localhost:3000/api/v1/profile',
+          params: { user: { name: 'Murilo' } },
+          as: :json,
+          headers: { Authorization: 'Token ADMI123456' }
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
